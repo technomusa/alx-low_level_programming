6aab71af30ca9979bfa7410ecdc323f8a5f43fcb
technomusa <52075608+technomusa@users.noreply.github.com>
2022-07-01 19:29:28 +0000

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 /**
- * main - Main block
- * SizeOf
- * Return value: always (0):successful
- */
-
+* main - Main block
+* SizeOf
+* Return value: always (0):successful
+*/
 int main(void){
 char acharacter;
 int ainteger;
 long along;
 long long alonglong;
 float afloat;
-
 printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
 printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
 printf("Size of a long int: %lu byte(s)\n", sizeof(along));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
 printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
-
 return (0);
 }
