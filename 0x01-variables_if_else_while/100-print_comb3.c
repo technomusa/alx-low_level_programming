8de4b97d2e19d601cb9e60826d2a878f10c9d34a
technomusa <andrenartmusa@gmail.com>
2022-07-01 12:22:09 -0700

    smile

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,29 +16,19 @@
 *All your code should be in the main function
 *Return: 0
 */
-int main(void)
-{
+int main(void){
 int c;
 int d = 0;
-while(d < 10)
-{
+while(d < 10){
 c = 0;
-while(c < 10)
-{
-if(d != c && d < c)
-{
+while(c < 10){
+if(d != c && d < c){
 putchar('0' + d);
 putchar('0' + c);
-if(c + d != 17)
-{
+if(c + d != 17){
 putchar(',');
-putchar(' ');
-}
-}
-c++;
-}
-d++;
-}
+putchar(' ');}}
+c++;}
+d++;}
 putchar('\n');
-return (0);
-}
+return (0);}
