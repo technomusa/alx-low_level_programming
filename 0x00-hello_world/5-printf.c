e37bce7354e6f53c6efb2ca170ac323ab4bae61a
technomusa <52075608+technomusa@users.noreply.github.com>
2022-07-01 19:28:16 +0000

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
- * main -Main block 
- * Prints "with proper grammar, but the ouptu is a piece of art,"
- * Return: Always 0 (Success)
- */
-
+* main -Main block
+* Prints "with proper grammar, but the ouptu is a piece of art,"
+* Return: Always 0 (Success)
+*/
 int main(void)
-
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");		return (0);
-
 }
