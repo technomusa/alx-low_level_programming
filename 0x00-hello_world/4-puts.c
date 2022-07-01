7f03013a6e0e159f010e16f5e643a9d958fd551f
technomusa <52075608+technomusa@users.noreply.github.com>
2022-07-01 19:27:02 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- * main - main block
- * Write '"Programming is like building a multilingual puzzle"'
- * Return: 0(success) 
- */
+* main - main block
+* Write '"Programming is like building a multilingual puzzle"'
+* Return: 0(success)
+*/
 int main(void)
 {
-
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-
 }
